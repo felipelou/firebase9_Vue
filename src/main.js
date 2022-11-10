396cc7882d0d8bb278791c852ad6eb4bf938ae8a
Felipe Alejandro Rosas Mendoza <felipelou123499@gmail.com>
2022-11-10 15:58:10 -0600

    make it better

--- src/main.js
+++ src/main.js
@@ -3,4 +3,50 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBogNxoz4e2QzPT7RYgvC2vuzMUSnbGOEY",
+  authDomain: "fir-a34d8.firebaseapp.com",
+  projectId: "fir-a34d8",
+  storageBucket: "fir-a34d8.appspot.com",
+  messagingSenderId: "819518137059",
+  appId: "1:819518137059:web:893a8439e5d063db15dbd6"
+};
+firebase.initializeApp(firebaseConfig)
+var storage = firebase.storage();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, storage };
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log(user)
+      const usuarioActivo ={
+        email: user.email,
+        uid: user.uid
+      }
+     store.dispatch('detectarUsuario', usuarioActivo)
+     console.log(usuarioActivo)
+    // ...
+  } else {
+    console.log(user)
+    store.dispatch('detectarUsuario', user)
+    // User is signed out
+    // ...
+  } 
+});
+
 createApp(App).use(store).use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,42 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import { getAuth } from "firebase/auth";
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
     path: '/about',
     name: 'about',
+    meta: {requiresAuth: true},
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)){
+    const auth = getAuth();
+    const usuario = auth.currentUser;
+    console.log ('usuario desde router', usuario)
+    if(!usuario){
+      next({path: '/'})
+    }else{
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
